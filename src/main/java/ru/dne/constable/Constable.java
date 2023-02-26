b8ed977caff1d767628ad65914cdd5c9b094296e
Dmitry Lomov <drupal.doesnot.exists@mail.ru>
2023-02-26 13:36:16 +0300

    Initial commit

---
+++ src/main/java/ru/dne/constable/Constable.java
@@ -0,0 +1,18 @@
+package ru.dne.constable;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Constable extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
