3afbd411187907682322f860aa47b9a4ce1516cd
Dmitry Lomov <drupal.doesnot.exists@mail.ru>
2023-02-26 15:31:57 +0300

    Implement configuration

--- src/main/java/ru/dne/constable/Constable.java
+++ src/main/java/ru/dne/constable/Constable.java
@@ -1,12 +1,22 @@
 package ru.dne.constable;
 
+import lombok.Getter;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Constable extends JavaPlugin {
 
+    @Getter
+    private static Constable constable;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        constable = this;
+
+        // Configuration
+        saveDefaultConfig();
 
     }
 
